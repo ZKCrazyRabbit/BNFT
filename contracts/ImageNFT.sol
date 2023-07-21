e9eb851fa79417a9f49212af6c0a6aaa35569e8b
mi_fei.ning <mi_fei.ning@mihoyo.com>
2023-07-21 19:00:05 +0800

    feat: 更新nft

--- contracts/ImageNFT.sol
+++ contracts/ImageNFT.sol
@@ -8,42 +8,30 @@ contract ImageNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    // TODO: 使用外部函数维护
-    constructor() ERC721("Crazy Rabbit", "IMGNFT") {
-        _images[1] = Image(
-            "https://bafybeidvjunwyuncwhfhd4ehkv7fggdg7ncglxizgflhtdgax2kftdzw5a.ipfs.w3s.link/nft_1.json"
-        );
-        _images[2] = Image(
-            "https://bafybeifmdw5z4yx2a5akxafvvfejkygzusvwgrm5wdow5ije3p66dfr6di.ipfs.w3s.link/nft_2.json"
-        );
-        _images[3] = Image(
-            "https://bafybeidietfaocihiwlqof4ifgs2d5lqkyujuathumtbswdybao655weo4.ipfs.w3s.link/nft_3.json"
-        );
-        _images[4] = Image(
-            "https://bafybeiboudii4gb2q6dfbrayx57wwxohod7fs3eifpb5rimhov3mzxhnpa.ipfs.w3s.link/nft_4.json"
-        );
-    }
+    constructor() ERC721("Crazy Rabbit", "IMGNFT") {}
 
     struct Image {
+        uint256 id;
         string ipfsHash;
     }
 
-    // TODO: 增加upsert、list、delete方法
     mapping(uint256 => Image) private _images;
+    Image[] private _image_list;
 
     // 手续费价格
     uint256 private _gasPrice;
 
     // 白名单用户
-    // TODO: 增加list、upsert、delete方法
-    mapping(address => uint) private _whitelist;
+    mapping(address => bool) private _whitelist;
+    address[] private _whitelist_arr;
 
     // mint事件通知
     event Mint(address indexed from, uint256 value);
 
     function mint(uint256 imageId) public payable returns (uint256) {
-        require(imageId > 0 && imageId <= 4, "Invalid image id");
-        Image memory image = _images[imageId];
+        require(imageId > 0, "Invalid image id");
+        Image memory image = getImageById(imageId);
+        require(isStringNotEmpty(image.ipfsHash), "Image is not exists");
 
         _tokenIds.increment();
         uint256 newImageId = _tokenIds.current();
@@ -56,22 +44,85 @@ contract ImageNFT is ERC721URIStorage {
         return newImageId;
     }
 
+    function listImage() public view returns (Image[] memory) {
+        return _image_list;
+    }
+
+    function upsertImage(uint256 id, string calldata ipfshash) public {
+        Image memory image = _images[id];
+        Image memory newImage = Image(id, ipfshash);
+        if (isStringNotEmpty(image.ipfsHash)) {
+            int256 i = findImageIndex(id);
+            if (i >= 0) {
+                removeImageByIndex(uint(i));
+            }
+        }
+        _image_list.push(newImage);
+        _images[id] = newImage;
+    }
+
+    function removeImage(uint256 id) public {
+        delete _images[id];
+        int256 i = findImageIndex(id);
+        if (i >= 0) {
+            removeImageByIndex(uint(i));
+        }
+    }
+
+    function findImageIndex(uint256 id) private view returns (int256) {
+        for (uint i = 0; i < _image_list.length; i++) {
+            if (_image_list[i].id == id) {
+                return int256(i);
+            }
+        }
+        return -1;
+    }
+
+    function removeImageByIndex(uint i) private {
+        _image_list[i] = _image_list[_image_list.length - 1];
+        _image_list.pop();
+    }
+
     function getImageById(uint256 imageId) public view returns (Image memory) {
-        require(imageId > 0 && imageId <= 4, "Invalid image id");
         Image memory image = _images[imageId];
         return image;
     }
 
     function getGasPrice() public view returns (uint256) {
-        // 判断是否为白名单用户
-        if (_whitelist[msg.sender] == 1) {
+        if (_whitelist[msg.sender] == true) {
             return 0;
         }
         return _gasPrice;
     }
 
-    // 获取gas price的原始内容
     function getOriginGasPrice() public view returns (uint256) {
         return _gasPrice;
+    }
+
+    function updateGasPrice(uint256 price) public {
+        _gasPrice = price;
+    }
+
+    function listWhiteList() public view returns (address[] memory) {
+        return _whitelist_arr;
+    }
+
+    function updateWhiteList(address[] calldata whitelist) public {
+        for (uint i = 0; i < _whitelist_arr.length; i++) {
+            delete _whitelist[_whitelist_arr[i]];
+        }
+        _whitelist_arr = whitelist;
+        for (uint i = 0; i < whitelist.length; i++) {
+            _whitelist[_whitelist_arr[i]] = true;
+        }
+    }
+
+    function isStringNotEmpty(string memory str) private pure returns (bool) {
+        bytes memory strBytes = bytes(str);
+        if (strBytes.length == 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
